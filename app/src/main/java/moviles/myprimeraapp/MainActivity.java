deccc7a8557d3f3c9f2a989b62347cb135666327
OscarLaiton2105 <143578914+OscarLaiton2105@users.noreply.github.com>
2023-09-10 00:43:56 -0500

    Agregue imagen,texto en español y icono

--- app/src/main/java/moviles/myprimeraapp/MainActivity.java
+++ app/src/main/java/moviles/myprimeraapp/MainActivity.java
@@ -10,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Verifica el nombre de usuario y la contraseña aquí
+
+                // Si la autenticación es exitosa, navega a la ventana 1
+                Intent intent = new Intent(MainActivity.this, Window1Activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
